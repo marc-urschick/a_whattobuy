04614187f2a52a92948980a2745fcc0911e19096
Wolfgang <brainme@aon.at>
2017-03-04 10:40:08 +0100

    Store als POJO eingefügt, da im ersten Frame die Liste aus Stores angezeigt werden soll. Beispiel: Media Markt, Cosmos, .....

--- app/src/main/java/com/wgmc/whattobuy/pojo/ShoppingList.java
+++ app/src/main/java/com/wgmc/whattobuy/pojo/ShoppingList.java
@@ -14,6 +14,19 @@ public class ShoppingList {
     private Shop whereToBuy;
     private List<Item> items;
     private Date dueTo;
+    private List<Store> store;
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public List<Store> getStore() {
+        return store;
+    }
+
+    public void setStore(List<Store> store) {
+        this.store = store;
+    }
 
     public ShoppingList(long id) {
         this.id = id;
