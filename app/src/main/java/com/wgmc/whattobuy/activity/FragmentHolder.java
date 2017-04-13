b3a891984d9beac733863741413d522efa5c0ad2
Wolfgang <brainme@aon.at>
2017-04-13 22:22:33 +0200

    Dimension Kommentare eingefügt

--- app/src/main/java/com/wgmc/whattobuy/activity/FragmentHolder.java
+++ app/src/main/java/com/wgmc/whattobuy/activity/FragmentHolder.java
@@ -4,7 +4,8 @@ import com.wgmc.whattobuy.fragment.BuylistDetailFragment;
 import com.wgmc.whattobuy.fragment.BuylistListFragment;
 
 /**
- * Created by proxie on 3.4.17.
+ * Created by proxie and poidl on 3.4.17.
+ it is a class that handle the shopping list. When detail list is open which list is open; This is important for changing orientation
  */
 
 public class FragmentHolder {
